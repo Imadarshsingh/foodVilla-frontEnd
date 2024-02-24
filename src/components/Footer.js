d95528a96a23352643228e6998b12ff9b7e1c86d
Adarsh Singh <adysingh1314@gmail.com>
2024-02-25 01:09:43 +0530

    little update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ const Footer=()=>{
         <div className="footer d-flex justify-content-center align-items-center" style={{borderTop:"2px solid white"}}>
         <div className="footer-info">
           <p className="p-size increased-font-size text-success fs-5">
-          Copyright ©2024<span className="text-white">FoodZilla</span> All Rights Reserved
+          Copyright ©2024 <span className="text-white">FoodZilla.</span> All Rights Reserved
           </p>
         </div>
       </div>
